4de42eff98374b50b2857ac74898ab2a9b24c79b
Youssef Ibrahim <87809813+youssefhossam12@users.noreply.github.com>
2026-03-14 19:08:06 -0400

    Added Giant Jam letters system (#2)

--- game_splitscreen.gd
+++ game_splitscreen.gd
@@ -8,3 +8,11 @@ func _ready() -> void:
 	viewport_2.world_2d = viewport_1.world_2d
 	player_2.camera.custom_viewport = viewport_2
 	player_2.camera.make_current()
+
+
+func _on_player_1_letter_collected(letter: String) -> void:
+	pass # Replace with function body.
+
+
+func _on_player_2_letter_collected(letter: String) -> void:
+	pass # Replace with function body.
